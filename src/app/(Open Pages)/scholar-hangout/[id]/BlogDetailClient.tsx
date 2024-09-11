68c77f0388649da75fe6209dc83c84b69a0d4a27
sabil <sabil.danish1997@gmail.com>
2024-09-11 23:49:03 +0530

    added title to blog pages

--- src/app/(Open Pages)/scholar-hangout/[id]/BlogDetailClient.tsx
+++ src/app/(Open Pages)/scholar-hangout/[id]/BlogDetailClient.tsx
@@ -6,10 +6,12 @@ import "./BlogDetails.css";
 const BlogDetailsClient = () => {
   const [url, setUrl] = useState<string | null>(null);
   const [excerpt, setExcerpt] = useState<any>()
+  const [title, setTitle] = useState<string | null>()
 
   useEffect(() => {
     setUrl(localStorage.getItem('url'));
     setExcerpt(localStorage.getItem('content'))
+    setTitle(localStorage.getItem("title"))
   }, []);
 
   console.log({url1: url,excerpt})
@@ -18,7 +20,10 @@ const BlogDetailsClient = () => {
     <div className="container">
       <div className="iframe-wrapper" id="iframe-wrapper">
         {excerpt ? (
+          <div>
+            <h1>{title}</h1>
           <div dangerouslySetInnerHTML={{__html: excerpt}}/>
+          </div>
         ) : (
           <p>Loading...</p>
         )}
