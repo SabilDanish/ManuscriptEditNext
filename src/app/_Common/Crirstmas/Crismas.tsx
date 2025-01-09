550a1f40893036882c6e9236094503db71371776
Developer_Biswa <biswaranjanpradhan@reseapro.com>
2025-01-09 11:13:34 +0530

    Before getting pull from Umesh

--- src/app/_Common/Crirstmas/Crismas.tsx
+++ src/app/_Common/Crirstmas/Crismas.tsx
@@ -5,10 +5,11 @@
 const ChritmasImage = () => {
   return (
     <>
-      <div className="floating_btn1">
-        {/* <img src="/images/58wu_p4n6_221019-removebg-preview.png" alt="#" /> */}
-        {/* <p className="text_icon">Talk to us?</p> */}
-      </div>
+      {/* <div className="floating_btn1">
+        <img src="/images/58wu_p4n6_221019-removebg-preview.png" alt="#" />
+        <p className="text_icon">Talk to us?</p>
+      </div> */}
+      
     </>
   )
 }
