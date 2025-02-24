6ae3718d2b0b6ae3c59c5abd1c6241965af90c79
Umesh <umeshpradhan.bls@gmail.com>
2025-02-24 11:19:05 +0530

    Have updated the pages upto editing services

--- src/app/_Common/Navbar2025/menu.tsx
+++ src/app/_Common/Navbar2025/menu.tsx
@@ -25,7 +25,7 @@ const menu: MenuItem[]  = [
           { label: "Substantive Editing", link: "/SubEditing" },
           { label: "Extensive Substantive Editing", link: "/ExtensiveSubstantiveEditing" },
           { label: "Plagiarism Check and Reduction", link: "/PlagiarismCheckReduction" },
-          { label: "A.I Reduction", link: "/EditingOverview" },
+          { label: "A.I Reduction", link: "/AIReduction" },
           { label: "Translation Services", link: "/TranslationOverview" },
         ],
       },
